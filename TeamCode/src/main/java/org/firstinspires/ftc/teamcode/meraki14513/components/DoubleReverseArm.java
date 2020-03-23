320429d1160e7e55bccde7364c9a106a7bc05f6a
lilih <2024mar49@moundsviewschools.org>
2020-03-22 20:20:34 -0500

    From Lulu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleReverseArm.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.meraki14513.components;
+
+public final class DoubleReverseArm extends BaseComponent {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
