e2ddbef222057f393967e71e46323457a21e0aa3
Ellen <springmn2015@gmail.com>
2020-03-22 20:36:16 -0500

    linear arm 1_2_16_Chain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.meraki14513.components;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public final class LinearArm1_2_16_Chain extends BaseComponent {
+    DcMotor motor;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
