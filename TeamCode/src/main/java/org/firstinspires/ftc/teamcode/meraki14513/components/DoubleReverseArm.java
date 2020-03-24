0f423bd00792a89c611b035f8baa40b3102a8e7f
lilih <2024mar49@moundsviewschools.org>
2020-03-24 13:22:43 -0500

    From Lulu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleReverseArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleReverseArm.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.meraki14513.components;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public final class DoubleReverseArm extends BaseComponent {
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
     @Override
     public void init() {
 
@@ -11,3 +16,4 @@ public final class DoubleReverseArm extends BaseComponent {
 
     }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleRollingHand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleRollingHand.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode.meraki14513.components;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public final class DoubleRollingHand extends BaseComponent {
+    private DcMotor motorLeftFront;
+
+    public DoubleRollingHand() {
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    public void move(int x, int y) {
+
+    }
+
+/**
+ * lift
 }
