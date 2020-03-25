49ecd3308090e4afe8ca28fded9abd86b5f12ddb
Ellen <springmn2015@gmail.com>
2020-03-24 19:58:11 -0500

    linear arm 1_2_16_Chain, double claw hand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode.meraki14513.components;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public final class LinearArm1_2_16_Chain extends BaseComponent {
-    DcMotor motor;
+    DcMotor RightMotor;
+    DcMotor LeftMotor;
 
     @Override
     public void init() {
-
+        RightMotor = hardwareMap.get(DcMotor.class, "huhhhhh");
+        LeftMotor = hardwareMap.get(DcMotor.class, "huhhhhhh");
     }
 
     @Override
