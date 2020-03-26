6d402758264390a5e3f2ca39c36f02ca70120e53
Emily <2023zhaoe83@gmail.com>
2020-03-25 21:09:10 -0500

    linear arm, double claw hand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.meraki14513.components;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 public final class LinearArm1_2_16_Chain extends BaseComponent {
     DcMotor LinearChainMotor;
@@ -13,7 +14,8 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
 
     @Override
     public void loop() {
-
+        LinearChainMotorPower = Range.clip(-1.0, 1.0) ;
+        LinearChainMotor.setPower(LinearChainMotorPower);
     }
     public void move(int x, int y, int z){
 
