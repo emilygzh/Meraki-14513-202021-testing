b4e4f77ea902aa2e092d8b6eab8a04c618842ecb
Emily <2023zhaoe83@gmail.com>
2020-03-26 20:32:00 -0500

    linear arm, double claw hand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -4,20 +4,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 public final class LinearArm1_2_16_Chain extends BaseComponent {
-    DcMotor LinearChainMotor;
+    public static final long MINIMUM =3001;
+    public static final long MAXIMUM = 8001;
+
+    private DcMotor LinearChainMotor;
+    private long currentPosition
 
     @Override
     public void init() {
         LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearChain_Motor");
-        telemetry.addData("Status", "Initialized");
     }
 
     @Override
     public void loop() {
-        int LinearChainMotorPower = Range.clip(-1.0, 1.0);;
-        LinearChainMotor.setPower(LinearChainMotorPower);
-    }
-    public void move(int x, int y, int z){
 
+    }
+    public void expand(long increment) {
+        currentPosition = currentPosition + increment;
+        if(currentPosition < MINIMUM) {
+            currentPosition = MINIMUM;
+        } else if(currentPosition > MAXIMUM) {
+            currentPosition = MAXIMUM;
+        }
+        LinearChainMotor.setPower(0.5);
+        try{
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LinearChainMotor.setPower(0)
     }
 }
