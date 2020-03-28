2b240d480434ec7b2ab84229b9df438837410a16
mxiao <mxiao0812@gmail.com>
2020-03-28 12:34:52 -0500

    From Maggie.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_Chain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.meraki14513.components;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 public final class LinearArm2_3_Chain extends BaseComponent {
     public static final long MINIMUM = 300l;
@@ -12,11 +13,13 @@ public final class LinearArm2_3_Chain extends BaseComponent {
     @Override
     public void init() {
         motorOne = hardwareMap.get(DcMotor.class, "left_drive");
+        telemetry.addData("Status", "Initialized");
     }
 
     @Override
     public void loop() {
-
+        int motorOne = Range.clip(-1.0, 1.0);
+        motorOne.setPower(motorOnePower);
     }
 
     public void expand(long increment) {
