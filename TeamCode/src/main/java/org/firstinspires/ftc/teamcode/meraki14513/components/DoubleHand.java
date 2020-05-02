c184d096981afc338e00bccb77731b1f0239a140
2023fengg37 <58153864+2023fengg37@users.noreply.github.com>
2020-05-02 18:45:05 -0500

    FTC arm F

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleHand.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.meraki14513.components;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class DoubleHand extends BaseComponent {
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
+    @Override
+    public void init() {
+
+    }
+
+
+    @Override
+    public void loop() {
+
+    }
+}
