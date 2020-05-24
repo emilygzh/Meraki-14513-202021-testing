59e4f4e96c34454c359d546c084af13ba53f6851
KAINISE <amandadiao16@gmail.com>
2020-05-24 18:51:37 -0500

    Completed Code for Chain Drive Train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleRollingHand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DoubleRollingHand.java
@@ -23,5 +23,3 @@ public final class DoubleRollingHand extends BaseComponent {
 
     }
 }
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.meraki14513.components;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
+@TeleOp(name="LinearArm1_2_16_Chain", group="Meraki 14513")
 public final class LinearArm1_2_16_Chain extends BaseComponent {
     public static final long MINIMUM=3001;
     public static final long MAXIMUM=8001;
@@ -13,8 +15,8 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
 
     @Override
     public void init() {
-        LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearChain_Motor");
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearArm1_Motor");
+        // LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         LinearChainMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
     }
@@ -22,7 +24,7 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
     @Override
     public void loop() {
         expand((int)gamepad2.right_stick_y * 10);
-
+        telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
     }
 
     public void expand(long increment) {
@@ -33,12 +35,19 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
             currentPosition = MAXIMUM;
         }
 
-        LinearChainMotor.setPower(0.5);
+        if (increment > 0) {
+            LinearChainMotor.setPower(0.5);
+        } else if (increment < 0) {
+            LinearChainMotor.setPower(-0.5);
+        } else {
+            LinearChainMotor.setPower(0.0);
+        }
         try{
-            Thread.sleep(increment);
+            Thread.sleep(Math.abs(increment));
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            LinearChainMotor.setPower(0.0);
         }
-        LinearChainMotor.setPower(0.0);
     }
 }
