1c8123ec5c83b32976554c0fd6fc568cdbb4fbf9
2023fengg37 <58153864+2023fengg37@users.noreply.github.com>
2020-05-24 19:06:23 -0500

    FTC 2 servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -7,17 +7,18 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="LinearArm1_2_16_Chain", group="Meraki 14513")
 public final class LinearArm1_2_16_Chain extends BaseComponent {
-    public static final long MINIMUM=3001;
-    public static final long MAXIMUM=8001;
+    public static final int MINIMUM=3001;
+    public static final int MAXIMUM=8001;
 
     private DcMotor LinearChainMotor;
-    private long currentPosition;
+    private int currentPosition;
 
     @Override
     public void init() {
         LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearArm1_Motor");
-        // LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         LinearChainMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        currentPosition = LinearChainMotor.getCurrentPosition();
         telemetry.addData("Status", "Initialized");
     }
 
@@ -27,27 +28,29 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
         telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
     }
 
-    public void expand(long increment) {
+    public void expand(int increment) {
         currentPosition = currentPosition + increment;
+        telemetry.addData("position: ", currentPosition);
         if(currentPosition < MINIMUM) {
             currentPosition = MINIMUM;
         } else if(currentPosition > MAXIMUM) {
             currentPosition = MAXIMUM;
         }
-
-        if (increment > 0) {
-            LinearChainMotor.setPower(0.5);
-        } else if (increment < 0) {
-            LinearChainMotor.setPower(-0.5);
-        } else {
-            LinearChainMotor.setPower(0.0);
-        }
-        try{
-            Thread.sleep(Math.abs(increment));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            LinearChainMotor.setPower(0.0);
-        }
+       
+        LinearChainMotor.setTargetPosition(currentPosition);
+//        if (increment > 0) {
+//            LinearChainMotor.setPower(0.5);
+//        } else if (increment < 0) {
+//            LinearChainMotor.setPower(-0.5);
+//        } else {
+//            LinearChainMotor.setPower(0.0);
+//        }
+//        try{
+//            Thread.sleep(Math.abs(increment));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            LinearChainMotor.setPower(0.0);
+//        }
     }
 }
