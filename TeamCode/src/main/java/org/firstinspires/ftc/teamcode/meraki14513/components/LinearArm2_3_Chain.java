f2d9bd2a86157c4b46321d185879290341f7f5fb
xyd10 <xyd10@comcast.net>
2020-05-26 21:36:31 -0500

    Implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_Chain.java
@@ -10,41 +10,61 @@ public final class LinearArm2_3_Chain extends BaseComponent {
     public static final int MINIMUM = 0;
     public static final int MAXIMUM = 1200;
 
-    private DcMotor LinearChainMotor;
-    private int currentPosition;
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private int leftPosition;
+    private int rightPosition;
 
     @Override
     public void init() {
-        LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearArm_Motor");
-        LinearChainMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LinearChainMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        currentPosition = LinearChainMotor.getCurrentPosition();
+        leftMotor = hardwareMap.get(DcMotor.class, "LinearArmLeft");
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftMotor.setPower(0.0);
+        leftPosition = leftMotor.getCurrentPosition();
+
+        rightMotor = hardwareMap.get(DcMotor.class, "LinearArmLeft");
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setPower(0.0);
+        rightPosition = rightMotor.getCurrentPosition();
+
         telemetry.addData("Status", "Initialized");
     }
 
     @Override
     public void loop() {
-        expand((int)gamepad2.right_stick_y * 30);
+        expandTogether((int)gamepad2.left_stick_y * 30);
         telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
     }
 
-    public void expand(int increment) {
-        currentPosition = currentPosition + increment;
-        telemetry.addData("position: ", currentPosition);
-        if(currentPosition < MINIMUM) {
-            currentPosition = MINIMUM;
-        } else if(currentPosition > MAXIMUM) {
-            currentPosition = MAXIMUM;
+    public void expandTogether(int increment) {
+        leftPosition = leftPosition + increment;
+        telemetry.addData("Target position: ", leftPosition);
+        if(leftPosition < MINIMUM) {
+            leftPosition = MINIMUM;
+        } else if(leftPosition > MAXIMUM) {
+            leftPosition = MAXIMUM;
         }
+        rightPosition = leftPosition;
 
-        LinearChainMotor.setTargetPosition(currentPosition);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        LinearChainMotor.setPower(1.0);
-        while (LinearChainMotor.isBusy()) {
-            telemetry.addData("currentposition", LinearChainMotor.getCurrentPosition());
+        leftMotor.setTargetPosition(leftPosition);
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setTargetPosition(rightPosition);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftMotor.setPower(1.0);
+        rightMotor.setPower(1.0);
+
+        while (leftMotor.isBusy() || rightMotor.isBusy()) {
+            telemetry.addData("Current position",
+                    leftMotor.getCurrentPosition() + " | " + rightMotor.getCurrentPosition());
         }
-        LinearChainMotor.setPower(0.0);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setPower(0.0);
+        rightMotor.setPower(0.0);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
