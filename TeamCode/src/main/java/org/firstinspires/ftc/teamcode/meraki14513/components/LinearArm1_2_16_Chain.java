90bc507f49db4bc939529c9891d10e0c90d231c3
mxiao <mxiao0812@gmail.com>
2020-05-31 19:41:52 -0500

    From Maggie.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm1_2_16_Chain.java
@@ -3,23 +3,22 @@ package org.firstinspires.ftc.teamcode.meraki14513.components;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="LinearArm1_2_16_Chain", group="Meraki 14513")
 public final class LinearArm1_2_16_Chain extends BaseComponent {
     public static final int MINIMUM = 0;
     public static final int MAXIMUM = 1200;
 
-    private DcMotor LinearChainMotor;
+    private DcMotor linearChainMotor;
     private int currentPosition;
 
     @Override
     public void init() {
-        LinearChainMotor = hardwareMap.get(DcMotor.class, "LinearArm1_Motor");
-        LinearChainMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LinearChainMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        currentPosition = LinearChainMotor.getCurrentPosition();
+        linearChainMotor = hardwareMap.get(DcMotor.class, "LinearArm1_Motor");
+        linearChainMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearChainMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        currentPosition = linearChainMotor.getCurrentPosition();
         telemetry.addData("Status", "Initialized");
     }
 
@@ -30,35 +29,24 @@ public final class LinearArm1_2_16_Chain extends BaseComponent {
     }
 
     public void expand(int increment) {
-        currentPosition = currentPosition + increment;
-        telemetry.addData("position: ", currentPosition);
-        if(currentPosition < MINIMUM) {
-            currentPosition = MINIMUM;
-        } else if(currentPosition > MAXIMUM) {
-            currentPosition = MAXIMUM;
-        }
+        if(increment < 1) {
+            linearChainMotor.setPower(0.0);
+        } else {
+            currentPosition = currentPosition + increment;
+            telemetry.addData("position: ", currentPosition);
+            if (currentPosition < MINIMUM) {
+                currentPosition = MINIMUM;
+            } else if (currentPosition > MAXIMUM) {
+                currentPosition = MAXIMUM;
+            }
 
-        LinearChainMotor.setTargetPosition(currentPosition);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        LinearChainMotor.setPower(1.0);
-        while (LinearChainMotor.isBusy()) {
-            telemetry.addData("currentposition", LinearChainMotor.getCurrentPosition());
+            linearChainMotor.setTargetPosition(currentPosition);
+            linearChainMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            linearChainMotor.setPower(1.0);
+            while (linearChainMotor.isBusy()) {
+                telemetry.addData("currentposition", linearChainMotor.getCurrentPosition());
+            }
+            linearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-        LinearChainMotor.setPower(0.0);
-        LinearChainMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        if (increment > 0) {
-//            LinearChainMotor.setPower(0.5);
-//        } else if (increment < 0) {
-//            LinearChainMotor.setPower(-0.5);
-//        } else {
-//            LinearChainMotor.setPower(0.0);
-//        }
-//        try{
-//            Thread.sleep(Math.abs(increment));
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-//            LinearChainMotor.setPower(0.0);
-//        }
     }
 }
