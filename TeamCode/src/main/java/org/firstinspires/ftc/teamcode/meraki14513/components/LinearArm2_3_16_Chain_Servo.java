0391b2138e72e978790399828763591dbd874c58
Emily <2023zhaoe83@gmail.com>
2020-06-19 15:26:51 -0500

    linear arm, double claw hand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_16_Chain_Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_16_Chain_Servo.java
@@ -30,14 +30,14 @@ public class LinearArm2_3_16_Chain_Servo extends BaseComponent{
             if(gamepad2.right_stick_x < 0.0) {
                 power = -1.0;
             }
-            expandByTogether(power, milliseconds);
+            expandByTogether(power)
         }
         else if(gamepad2.b) {
-            expandByTogether(1.0, 2000);
+            expandByTogether(1.00);
             // maximum positive power and time
         }
         else if(gamepad2.x) {
-            expandByTogether(-1.0, 2000);
+            expandByTogether(-1.0);
             // maximum negative power and time
         }
         else {
@@ -46,14 +46,11 @@ public class LinearArm2_3_16_Chain_Servo extends BaseComponent{
         telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
     }
 
-    public void expandByTogether(double power, int milliseconds) {
-        milliseconds = (int)(target/power);
-        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    public void expandByTogether(double power) {
         leftServo.setPower(power);
         rightServo.setPower(power);
     }
 
     public void stop() {
-        CRServo.setPower(0.0);
-    }
+        CRServo.setPower(0.0); }
 }
