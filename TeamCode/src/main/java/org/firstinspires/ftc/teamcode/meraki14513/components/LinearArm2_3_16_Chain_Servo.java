113a921ee1cfa36e6f68b216d738e279980dd1d6
mxiao <mxiao0812@gmail.com>
2020-06-19 15:26:00 -0500

    From Maggie.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_16_Chain_Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/LinearArm2_3_16_Chain_Servo.java
@@ -25,19 +25,18 @@ public class LinearArm2_3_16_Chain_Servo extends BaseComponent{
     public void loop() {
         // change these controls to proper ones to avoid simultaneous moving of arms
         if(gamepad2.right_stick_x != 0.0) {
-            int milliseconds = (int) Math.abs(gamepad2.left_stick_button * 100);
             double power = 1.0;
             if(gamepad2.right_stick_x < 0.0) {
                 power = -1.0;
             }
-            expandByTogether(power, milliseconds);
+            expandByTogether(power);
         }
         else if(gamepad2.b) {
-            expandByTogether(1.0, 2000);
+            expandByTogether(1.0);
             // maximum positive power and time
         }
         else if(gamepad2.x) {
-            expandByTogether(-1.0, 2000);
+            expandByTogether(-1.0);
             // maximum negative power and time
         }
         else {
@@ -46,9 +45,7 @@ public class LinearArm2_3_16_Chain_Servo extends BaseComponent{
         telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
     }
 
-    public void expandByTogether(double power, int milliseconds) {
-        milliseconds = (int)(target/power);
-        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    public void expandByTogether(double power) {
         leftServo.setPower(power);
         rightServo.setPower(power);
     }
