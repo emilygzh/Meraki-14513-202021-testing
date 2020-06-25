d0bff71d2ffc81e04855a0cdef9d5a78dd4d13f2
Claire <2023lic14@gmail.com>
2020-06-24 20:07:07 -0500

    Changed stuff added controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DriveTrain4Mecanum75.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/meraki14513/components/DriveTrain4Mecanum75.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Drive train with four independent wheel and 16" x 16" H layout
@@ -40,20 +41,20 @@ public final class DriveTrain4Mecanum75 extends BaseComponent {
         leftRearDrive = hardwareMap.get(DcMotor.class, "Mecanum75.leftRearDrive");
         rightRearDrive = hardwareMap.get(DcMotor.class, "Mecanum75.rightRearDrive");
 
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontDrive.setPower(0.0);
+        rightFrontDrive.setPower(0.0);
+        leftRearDrive.setPower(0.0);
+        rightRearDrive.setPower(0.0);
 
-        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftRearDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftFrontDrive.setPower(0.7);
-        rightFrontDrive.setPower(0.7);
-        leftRearDrive.setPower(0.7);
-        rightRearDrive.setPower(0.7);
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     /**
@@ -61,9 +62,28 @@ public final class DriveTrain4Mecanum75 extends BaseComponent {
      */
     @Override
     public void loop() {
-        move((int)gamepad1.left_stick_x * 10, (int)gamepad1.left_stick_y * 10);
-        turn((int)gamepad1.left_trigger * 10); // turn left
-        turn((int)gamepad1.right_trigger * -10); // turn right
+        int scale = 300;
+        if (gamepad1.left_stick_y != 0.0 || gamepad1.left_stick_x != 0.0) {
+            moveByTime(100,
+                    getPower(gamepad1.left_stick_x, gamepad1.left_stick_y),
+                    getPower(gamepad1.left_stick_x, gamepad1.left_stick_y),
+                    getPower(gamepad1.left_stick_x, gamepad1.left_stick_y),
+                    getPower(gamepad1.left_stick_x, gamepad1.left_stick_y));
+        } else if (gamepad1.left_trigger > 0.0) {
+            moveByTime(100, -gamepad1.left_trigger, gamepad1.left_trigger, -gamepad1.left_trigger, gamepad1.left_trigger);
+        } else if (gamepad1.right_trigger > 0.0) {
+            moveByTime(100, gamepad1.right_trigger, -gamepad1.right_trigger, gamepad1.right_trigger, -gamepad1.right_trigger);
+        }
+    }
+
+    private double getPower(double x, double y) {
+        double power = Math.sqrt(x*x + y*y);
+        if ((x < 0.0 && y > 0.0) || (x > 0.0 && y < 0.0)) {
+            return -power;
+        }
+        else {
+            return power;
+        }
     }
 
     /**
@@ -91,5 +111,21 @@ public final class DriveTrain4Mecanum75 extends BaseComponent {
         rightFrontDrive.setTargetPosition(rightFrontDrive.getCurrentPosition() - leftRightIncrement);
         leftRearDrive.setTargetPosition(leftRearDrive.getCurrentPosition() + leftRightIncrement);
         rightRearDrive.setTargetPosition(rightRearDrive.getCurrentPosition() - leftRightIncrement);
+    }
+
+    public void moveByTime(long milliseconds,
+                           double leftFrontPower, double rightFrontPower, double leftRearPower, double rightRearPower) {
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftRearDrive.setPower(leftRearPower);
+        rightRearDrive.setPower(rightRearPower);
+        ElapsedTime millitime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        while (millitime.milliseconds() < milliseconds) {
+            telemetry.addData("millitime", millitime.milliseconds());
+        }
     }
 }
